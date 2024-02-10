31bad458b83eb617c62f881bda15372d4b2f1787
Riteeka <riteekagawand7@gmail.com>
2024-02-10 06:25:42 +0530

    first commit

---
+++ Statments/stat.js
@@ -0,0 +1,7 @@
+//Statements in JavaScript.
+
+let a,b,c; //variables can be declared in variables ways using var let const
+a = 5; // here a is the variable and 5 is its value.
+b= 4;
+c= a+b
+ console.log(c)
